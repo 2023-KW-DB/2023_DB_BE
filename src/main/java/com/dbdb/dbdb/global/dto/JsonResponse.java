aef685b57007cab42d76204844b99a09122bba6c
방지민 <bjimin7@naver.com>
2023-11-11 23:15:45 +0900

    feat: Modify the excursion handlers

--- src/main/java/com/dbdb/dbdb/global/dto/JsonResponse.java
+++ src/main/java/com/dbdb/dbdb/global/dto/JsonResponse.java
@@ -1,5 +1,6 @@
 package com.dbdb.dbdb.global.dto;
 
+import com.dbdb.dbdb.global.exception.ResponseStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,13 +8,37 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import static com.dbdb.dbdb.global.exception.ResponseStatus.SUCCESS;
+
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class JsonResponse {
+public class JsonResponse <T> {
     private final String timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     private boolean isSuccess;
     private int status;
     private String message;
     private Object result;
+
+    // 요청에 성공한 경우
+    public JsonResponse(T result){
+        this.isSuccess = SUCCESS.isSuccess();
+        this.message = SUCCESS.getMessage();
+        this.status = SUCCESS.getCode();
+        this.result = result;
+    }
+
+    // 요청에 성공한 경우 + 커스텀 메시지
+    public JsonResponse(ResponseStatus status, T result){
+        this.isSuccess = status.isSuccess();
+        this.message = status.getMessage();
+        this.status = status.getCode();
+        this.result = result;
+    }
+
+    // 요청에 실패한 경우
+    public JsonResponse(ResponseStatus status){
+        this.isSuccess = status.isSuccess();
+        this.message = status.getMessage();
+        this.status = status.getCode();
+        this.result = null;
+    }
 }
--- src/main/java/com/dbdb/dbdb/global/exception/GlobalException.java
+++ src/main/java/com/dbdb/dbdb/global/exception/GlobalException.java
@@ -6,5 +6,5 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class GlobalException extends RuntimeException {
-    private ErrorStatus status;
+    private ResponseStatus status;
 }
