f6e6d816dcd07086036af281a6e61aa68cfdf05f
bbangjun <lucid9dawn@gmail.com>
2023-11-18 02:16:31 +0900

    feat(KakaoLoginService): 액세스 토큰 response를 반환하는 서비스 로직 구현

--- src/main/java/com/dbdb/dbdb/service/KakaoLoginService.java
+++ src/main/java/com/dbdb/dbdb/service/KakaoLoginService.java
@@ -22,5 +22,40 @@ public class KakaoLoginService {
     @Autowired
     private Environment env;
 
+    public JsonNode getAccessTokenResponse(String code) {
 
+        RestTemplate restTemplate = new RestTemplate();
+
+        // HttpHeader object 생성
+        HttpHeaders headers = new HttpHeaders();;
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HttpBody object 생성
+        String clientId = env.getProperty("oauth2.kakao.client-id");
+        String redirectUri = env.getProperty("oauth2.kakao.redirect-uri");
+        String resourceUri = env.getProperty("oauth2.kakao.resource-uri");
+
+        // 확인을 위한 log
+        log.info("clientId = {}", clientId);
+        log.info("redirectUri = {}", redirectUri);
+        log.info("resourceUri = {}", resourceUri);
+
+        // HttpHeader와 HttpBody를 하나의 obejct에 담기
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", clientId);
+        params.add("redirect_uri", redirectUri);
+        params.add("code", code);
+
+        HttpEntity<MultiValueMap<String, String>> accessTokenRequest = new HttpEntity<>(params, headers);
+
+        JsonNode response = restTemplate.exchange(
+                resourceUri,
+                HttpMethod.POST,
+                accessTokenRequest,
+                JsonNode.class
+        ).getBody();
+
+        return response;
+    }
 }
