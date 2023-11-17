9203a085a21d8f389b3fa62ba4c232f33c2896b3
bbangjun <lucid9dawn@gmail.com>
2023-11-18 01:47:41 +0900

    feat(KakaoLoginController): 카카오 로그인 시 콜백 메서드 구현

---
+++ src/main/java/com/dbdb/dbdb/controller/KakaoLoginController.java
@@ -0,0 +1,15 @@
+package com.dbdb.dbdb.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/login/oauth2", produces = "application/json")
+public class KakaoLoginController {
+
+    @GetMapping("/code/kakao/kakao")
+    public @ResponseBody String kakaoCallback(@RequestParam String code){
+        return code;
+    }
+}
