b66e495a589cb34b4be6798f9465824a90d9d6c1
bbangjun <lucid9dawn@gmail.com>
2023-11-18 02:04:29 +0900

    docs(build.gradle): oauth2 client 의존성 추가

---
+++ src/main/java/com/dbdb/dbdb/service/KakaoLoginService.java
@@ -0,0 +1,26 @@
+package com.dbdb.dbdb.service;
+
+import com.dbdb.dbdb.repository.UserRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Service
+public class KakaoLoginService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private Environment env;
+
+
+}
