3418126e54d538e2b2686467a41a61852fc354ed
방지민 <bjimin7@naver.com>
2023-11-18 04:28:00 +0900

    fix(data.sql): 인코딩 변경 및 데이터 정상 저장 확인

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,7 @@ INTO TABLE bikestationinformation
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES;
+IGNORE 1 LINES
+(lendplace_id, statn_addr1, statn_addr2, startn_lat, startn_lnt)
+SET max_stands = 20,
+    station_status = NULL;
