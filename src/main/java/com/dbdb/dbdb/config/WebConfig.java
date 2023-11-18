b0e9656458d613ed4ecc60029d4366135ed18899
김성진 <mirusu400@naver.com>
2023-11-18 13:08:32 +0900

    fix: add CORS Header for frontend webapp

---
+++ src/main/java/com/dbdb/dbdb/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.dbdb.dbdb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods(HttpMethod.GET.name())
+                .allowedMethods(HttpMethod.POST.name());
+
+    }
+}
