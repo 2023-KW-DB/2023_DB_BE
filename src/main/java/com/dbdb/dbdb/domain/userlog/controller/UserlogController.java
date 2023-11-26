ad1b397da44dcb1b39c3690aacb9b647bae825f2
bbangjun <lucid9dawn@gmail.com>
2023-11-26 16:38:33 +0900

    feat(FCMService.java): 자전거 대여, 자전거 반납 구현 완료

--- src/main/java/com/dbdb/dbdb/domain/userlog/controller/UserlogController.java
+++ src/main/java/com/dbdb/dbdb/domain/userlog/controller/UserlogController.java
@@ -1,7 +1,9 @@
 package com.dbdb.dbdb.domain.userlog.controller;
 
+import com.dbdb.dbdb.domain.user.service.UserService;
 import com.dbdb.dbdb.domain.userlog.dto.UserlogDto;
 import com.dbdb.dbdb.domain.userlog.service.UserlogService;
+import com.dbdb.dbdb.fcm.FCMService;
 import com.dbdb.dbdb.global.dto.JsonResponse;
 import com.dbdb.dbdb.global.exception.ResponseStatus;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,10 @@ public class UserlogController {
 
     @Autowired
     private UserlogService userlogService;
+    @Autowired
+    private FCMService fcmService;
+    @Autowired
+    private UserService userService;
 
     // 자전거 대여
     @PostMapping("/users/bike-rental")
@@ -22,13 +28,20 @@ public class UserlogController {
 
         String response = userlogService.bikeRental(userlogDto);
 
-        if(response=="FAILED_NO_VALID_TICKET")
+        if(response=="FAILED_NO_VALID_TICKET"){
+            fcmService.sendBikeRentalFailedMessage(userService.findUserEmailById(userlogDto.getUser_id()));
             return ResponseEntity.ok(new JsonResponse(ResponseStatus.FAILED_NO_VALID_TICKET, null));
-        else if(response=="FAILED_INVALID_RENTAL_STATION")
+        }
+        else if(response=="FAILED_INVALID_RENTAL_STATION"){
+            fcmService.sendBikeRentalFailedMessage(userService.findUserEmailById(userlogDto.getUser_id()));
             return ResponseEntity.ok(new JsonResponse(ResponseStatus.FAILED_INVALID_RENTAL_STATION, null));
-        else if(response=="FAILED_INVALID_BIKE")
+        }
+        else if(response=="FAILED_INVALID_BIKE"){
+            fcmService.sendBikeRentalFailedMessage(userService.findUserEmailById(userlogDto.getUser_id()));
             return ResponseEntity.ok(new JsonResponse(ResponseStatus.FAILED_INVALID_BIKE, null));
+        }
 
+        fcmService.sendBikeRentalSuccessMessage(userService.findUserEmailById(userlogDto.getUser_id()));
         return ResponseEntity.ok(new JsonResponse(ResponseStatus.SUCCESS_BIKE_RENTAL, null));
     }
 
@@ -38,11 +51,16 @@ public class UserlogController {
 
         String response = userlogService.bikeReturn(userlogDto);
 
-        if(response=="FAILED_INVALID_RETURN_STATION")
+        if(response=="FAILED_INVALID_RETURN_STATION"){
+            fcmService.sendBikeReturnFailedMessage(userService.findUserEmailById(userlogDto.getUser_id()));
             return ResponseEntity.ok(new JsonResponse(ResponseStatus.FAILED_INVALID_RETURN_STATION, null));
-        else if(response=="FAILED_OVER_MAX_STANDS")
+        }
+        else if(response=="FAILED_OVER_MAX_STANDS"){
+            fcmService.sendBikeReturnFailedMessage(userService.findUserEmailById(userlogDto.getUser_id()));
             return ResponseEntity.ok(new JsonResponse(ResponseStatus.FAILED_OVER_MAX_STANDS, null));
+        }
 
+        fcmService.sendBikeReturnSuccessMessage(userService.findUserEmailById(userlogDto.getUser_id()));
         return ResponseEntity.ok(new JsonResponse(ResponseStatus.SUCCESS_BIKE_RETURN, null));
     }
 }
