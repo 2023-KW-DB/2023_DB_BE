ea525c729dced1dbd28fafd324e9eb37bd71e55a
방지민 <bjimin7@naver.com>
2023-11-29 12:02:19 +0900

    fix(DatabaseInitializer): 존재하지 않을 때에만 assertion

--- src/main/java/com/dbdb/dbdb/config/DatabaseInitializer.java
+++ src/main/java/com/dbdb/dbdb/config/DatabaseInitializer.java
@@ -13,16 +13,23 @@ public class DatabaseInitializer implements CommandLineRunner {
         this.jdbcTemplate = jdbcTemplate;
     }
     @Override
-    public void run(String... args) throws Exception {
-        String createTrigger = "CREATE TRIGGER assertion_user " +
-                "BEFORE INSERT ON user " +
-                "FOR EACH ROW " +
-                "BEGIN " +
-                "    IF NEW.user_type NOT IN (0, 1, 2) THEN " +
-                "        SIGNAL SQLSTATE '45000' " +
-                "        SET MESSAGE_TEXT = 'user_type은 0, 1 or 2이어야 합니다'; " +
-                "    END IF; " +
-                "END;";
-        jdbcTemplate.execute(createTrigger);
+    public void run(String... args) {
+        // 트리거 존재 여부 확인
+        String checkTrigger = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.TRIGGERS WHERE TRIGGER_SCHEMA = ? AND TRIGGER_NAME = ?";
+        Integer count = jdbcTemplate.queryForObject(checkTrigger, new Object[]{"dbdb", "assertion_user"}, Integer.class);
+
+        // 트리거가 존재하지 않으면 생성
+        if (count == null || count == 0) {
+            String createTrigger = "CREATE TRIGGER assertion_user " +
+                    "BEFORE INSERT ON user " +
+                    "FOR EACH ROW " +
+                    "BEGIN " +
+                    "    IF NEW.user_type NOT IN (0, 1, 2) THEN " +
+                    "        SIGNAL SQLSTATE '45000' " +
+                    "        SET MESSAGE_TEXT = 'user_type은 0, 1 or 2이어야 합니다'; " +
+                    "    END IF; " +
+                    "END;";
+            jdbcTemplate.execute(createTrigger);
+        }
     }
 }
