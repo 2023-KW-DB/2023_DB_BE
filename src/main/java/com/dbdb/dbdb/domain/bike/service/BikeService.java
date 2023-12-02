27aabdbb0e448d9d807da76988b37861d4484763
방지민 <bjimin7@naver.com>
2023-12-03 04:38:01 +0900

    refactor(BikeService): Response Status 추가

--- src/main/java/com/dbdb/dbdb/domain/bike/service/BikeService.java
+++ src/main/java/com/dbdb/dbdb/domain/bike/service/BikeService.java
@@ -71,7 +71,7 @@ public class BikeService {
             bikeRepository.delete(bikeDeleteDto.getId());
 
         } catch (Exception e) {
-            throw e;//new GlobalException(ResponseStatus.DATABASE_ERROR);
+            throw new GlobalException(ResponseStatus.DATABASE_ERROR);
         }
     }
 
